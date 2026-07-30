0b5650a23f7caea53516c59f4690684df5fee275
Cristian Pardo <developer.ux22@gmail.com>
2026-07-30 01:18:10 -0500

    diseño arreglado

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function crearTarjeta(user) {
   const ciudad = document.createElement("p");
 
   div.className =
-    "neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs";
+    "max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md";
   nombre.className =
-    "mb-3 text-2xl font-semibold tracking-tight text-heading leading-8";
+    "mb-3 text-2xl font-semibold tracking-tight text-gray-900 leading-8 truncate";
+  correo.className = "text-gray-700 truncate";
+  ciudad.className = "text-gray-500 truncate";
 
   nombre.textContent = user.name;
   correo.textContent = user.email;
@@ -24,8 +26,9 @@ function crearTarjeta(user) {
 
 function mostrarError(message) {
   const p = document.createElement("p");
-  p.className = "text-red-500";
-  p.textContent = message;
+  p.className =
+    "p-4 mb-4 text-red-800 bg-red-100 border border-red-300 rounded-lg";
+  p.textContent = "⚠️ " + message;
 
   usersContainer.append(p);
 }
